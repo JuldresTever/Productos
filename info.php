5cb17b1301ee7df5de70f63171f5cd859f4328ba
JuldresTever <juliantellezv2007@gmail.com>
2024-08-08 15:58:27 -0500

    Update info.php

--- info.php
+++ info.php
@@ -1,1 +1,20 @@
+<?php
+$Produ1= $_POST ['Produ1'];
+$Produ2=$_POST ['Produ2'];
+$Produ3=$_POST ['Produ3'];
+$Precio1=$_POST ['Precio1'];
+$Precio2=$_POST ['Precio2'];
+$Precio3=$_POST ['Precio3'];
+$SumaPrecios= 0;
+$Descuento= 0;
+$Total= 0;
 
+$SumaPrecios= $Precio1 + $Precio2 + $Precio3;
+$Descuento= $SumaPrecios * 0,16;
+$Total= $SumaPrecios - $Descuento;
+
+echo "Los productos comprados son: ".$Produ1 .$Produ2 .$Produ3;
+echo "El precio de estos articulos es: ".$SumaPrecios;
+echo "Se hara un descuento de: ".$Descuento;
+echo "El total a pagar es: ".%Total;
+?> 
